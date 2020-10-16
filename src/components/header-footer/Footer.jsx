2a7f00028863600b150c0b5407fe476ba788c8fb
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-10-16 13:24:08 +0700

    configuration footer

--- src/components/header-footer/Footer.jsx
+++ src/components/header-footer/Footer.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Subscribe from './Subscribe';
 
 const Footer = () => {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer className="footer">
+      <div className="wrapper wrapper--footer">
+        <div className="footer__logos">
+          <div className="logo"></div>
+          <div className="universal-logo"></div>
+          <div className="footer__copy"></div>
+        </div>
+        <div className="footer__subscribe">
+          <h3>JOIN MAILING LIST</h3>
+          <Subscribe />
+        </div>
+        <div className="footer__social">
+          <h3>BE IN TOUCH WITH US</h3>
+        </div>
+      </div>
+    </footer>
   );
 };
 
