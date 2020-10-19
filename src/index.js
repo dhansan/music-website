831689c8feab1c14ce116f4ec7a1a0e76dc7bfa3
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-10-19 11:46:25 +0700

    configure path tour and shop

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Home from './components/home/Home';
 import About from './components/About';
 import './main.scss';
 import Discography from './components/Discography';
-// import Tour from './components/Tour';
-// import Shop from './components/shop/Shop';
+import Tour from './components/Tour';
+import Shop from './components/shop/Shop';
 import Footer from './components/header-footer/Footer';
 import Header from './components/header-footer/Header';
 
@@ -20,8 +20,8 @@ const Routes = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/discography" component={Discography} />
-      {/* <Route exact path="/tour" component={Tour} /> */}
-      {/* <Route exact path="/shop" component={Shop} /> */}
+      <Route exact path="/tour" component={Tour} />
+      <Route exact path="/shop" component={Shop} />
       <Footer />
     </div>
   </BrowserRouter>
