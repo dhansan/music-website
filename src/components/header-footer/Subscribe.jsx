b5ebd2bce62c073582d8730003493209970ff4e8
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-10-21 15:12:13 +0700

    fixing subcribe

--- src/components/header-footer/Subscribe.jsx
+++ src/components/header-footer/Subscribe.jsx
@@ -4,13 +4,13 @@ import Button from '../ui/Button';
 const Subscribe = () => {
   return (
     <div className="subscribe">
-      <input type="text" placeholder="Your email" />
-      <select>
+      <input type="text" name="" id="" placeholder="Your email" />
+      <select name="" id="">
         <option value="">Select your country</option>
       </select>
       <div className="subscribe__agreement">
-        <input type="checkbox" id="agreement" />
-        <label htmlFor="agreement">
+        <input type="checkbox" name="" id="agreement" />
+        <label htmlFor="agreement" className="">
           By submitting my information above, I acknowledge that I have reviewed
           and hereby agree to the Privacy Policy and Terms of Use.
         </label>
